1d63a4161e61210c1e112fda3d801607422f30b2
Josh-Tse <joshuaktse@gmail.com>
2023-09-24 23:16:43 +0000

    Create prsm.sh

---
+++ Personal Record Management System/prsm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#welcome prompt
+echo 'PERSONAL RECORD MANAGEMENT SYSTEM'
+echo -e "\n What would you like to do today? \n"
+echo -e "\n 1. Add New Record
+2. Edit Existing Records
+3. Search For Specific Records
+4. Generate Report \n"
+
+echo -e "Please Enter A Number To Select An Option.\n"
+read option 
+
+if [option==1]
+
+
